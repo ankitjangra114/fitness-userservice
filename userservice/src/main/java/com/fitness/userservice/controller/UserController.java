6a70e0cf0cf3b3bd5b9fb26040225b7b79978c03
Ankit Jangra <ankit04103@gmail.com>
2025-08-31 23:21:02 +0530

    Initial commit- fitness microservices project

---
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.fitness.userservice.controller;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.services.UserServices;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+    private UserServices userServices;
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request){
+        return ResponseEntity.ok(userServices.register(request));
+    }
+}
---
+++ userservice/src/main/java/com/fitness/userservice/services/UserServices.java
@@ -0,0 +1,35 @@
+package com.fitness.userservice.services;
+
+import com.fitness.userservice.UserRepository;
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.models.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserServices {
+    private final UserRepository repository;
+    public UserResponse register(RegisterRequest request) {
+        if(repository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("Email already exist");
+        }
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPassword(request.getPassword());
+
+        User savedUser = repository.save(user);
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdateAt());
+        return userResponse;
+    }
+}
